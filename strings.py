45f5b0a8be322a484086204021c411b8bc2d7abf
S458627 <S458627@shhac150M12.ocsb.local>
2022-05-11 12:08:40 -0400

    Eclipse Files

---
+++ strings.py
@@ -0,0 +1,30 @@
+"""
+Strings are a datatype.
+Strings can be changed.
+Strings are written within signle or double quotation marks.
+All user input are strings.
+"""
+
+
+string1 = "This is a string."
+#string2 = 'This is also a string.'
+
+print(string1)
+print(string1[5])
+print(string1[2:4])
+print(string1[3:8])
+print(string1[:])
+print(string1 * 2)
+
+
+
+var1 = ord('S')
+print(var1)
+
+var2 = chr(29)
+print(var2)
+
+
+#var1 = input("Please enter some text:")
+#print("You entered: " + var1)
+
